78b11ae38123bf27c0e882ddea83258600ca2492
Luther Isaboke <lusaboke@gmail.com>
2024-03-27 23:28:42 +0300

    func cont

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// implementing a function called saturdayFun
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+//implementing a function expression calledMonday work
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  //implemnting a function called wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function(adjective="special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  };
+  
